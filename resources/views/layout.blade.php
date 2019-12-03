543f750ab5a09fa273e7bcd61017b002a83605fa
Nwdles <lpsh.nt@gmail.com>
2019-12-04 00:57:28 +0300

    лишний тег скрипта

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <!-- Custom styles for this template -->
     <link href="css/album.css" rel="stylesheet">
-    <script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=rjvDfynsbS_0x208ad0guFccKT1s4bzMYCfG2n27ITaLJ5hNqsmHZAc1vVz8AvgSJyHWKUxSxpZueAuC8eDZAF6tZdxyIuaXLXpJxayYf3o" charset="UTF-8"></script></head>
+    
 <link href="css/carousel.css" rel="stylesheet">
 <body>
 
